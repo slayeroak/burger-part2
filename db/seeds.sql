3ef3d53df3f2dbb92dda902cc19ccb54bd457479
slayeroak <roakley1@villanova.edu>
2020-04-30 23:38:10 -0400

    npm installs and transferred over old burger

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+  
+INSERT INTO burgers (burger_name) VALUES ("cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("doublecheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("hamburger");
