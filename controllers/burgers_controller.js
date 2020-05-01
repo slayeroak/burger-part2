6f3ba77c00070d34655e2b086704de1bfbd0dd71
slayeroak <roakley1@villanova.edu>
2020-05-01 00:19:18 -0400

    Updated queries in burgerController for updating a burger to add the CustomerId

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,42 +1,84 @@
 var express = require("express");
 
 var router = express.Router();
+// grabbing our models
+var db = require("../models");
 
-var burgers = require("../models/burger.js");
-
-router.get("/", function(req,res){
-	res.redirect("burgers")
+// get route -> index
+router.get("/", function(req, res) {
+  // send us to the next get function instead.
+  res.redirect("/burgers");
 });
 
-router.get("/burgers", function(req,res){
-	burgers.selectAll(function(data){
-		var hbsObject = {
-			burgers: data
-		};
-		console.log(hbsObject);
-		res.render("index", hbsObject);
-	});
+// get route, edited to match sequelize
+router.get("/burgers", function(req, res) {
+  // replace old function with sequelize function
+  db.Burger.findAll({
+    include: [db.Customer],
+    // Here we specify we want to return our burgers in ordered by ascending burger_name
+    order: [
+      ["burger_name", "ASC"]
+    ]
+  })
+  // use promise method to pass the burgers...
+    .then(function(dbBurger) {
+    // into the main index, updating the page
+      var hbsObject = {
+        burger: dbBurger
+      };
+      return res.render("index", hbsObject);
+    });
 });
 
-router.post("/burgers/create", function(req,res){
-	burgers.insertOne([
-		"burger_name"
-		],[
-			req.body.burger_name
-			], function(data){
-				res.redirect("/burgers");
-			});
+// post route to create burgers
+router.post("/burgers/create", function(req, res) {
+  // edited burger create to add in a burger_name
+  db.Burger.create({
+    burger_name: req.body.burger_name
+  })
+  // pass the result of our call
+    .then(function(dbBurger) {
+    // log the result to our terminal/bash window
+      console.log(dbBurger);
+      // redirect
+      res.redirect("/");
+    });
 });
 
-router.put("/burgers/update/:id", function(req,res){
-	var condition = "id = " + req.params.id;
-	console.log("condition", condition);
-
-	burgers.updateOne({
-		"devoured": req.body.devoured
-	}, condition, function(data){
-		res.redirect("/burgers")
-	});
+// put route to devour a burger
+router.put("/burgers/update", function(req, res) {
+  // If we are given a customer, create the customer and give them this devoured burger
+  if (req.body.customer) {
+    db.Customer.create({
+      customer: req.body.customer,
+      BurgerId: req.body.burger_id
+    })
+      .then(function(dbCustomer) {
+        return db.Burger.update({
+          devoured: true
+        }, {
+          where: {
+            id: req.body.burger_id
+          }
+        });
+      })
+      .then(function(dbBurger) {
+        res.json("/");
+      });
+  }
+  // If we aren't given a customer, just update the burger to be devoured
+  else {
+    db.Burger.update({
+      devoured: true
+    }, {
+      where: {
+        id: req.body.burger_id
+      }
+    })
+      .then(function(dbBurger) {
+        res.json("/");
+      });
+  }
 });
 
 module.exports = router;
--- db/seeds.sql
+++
@@ -1,4 +0,0 @@
-  
-INSERT INTO burgers (burger_name) VALUES ("cheeseburger");
-INSERT INTO burgers (burger_name) VALUES ("doublecheeseburger");
-INSERT INTO burgers (burger_name) VALUES ("hamburger");
